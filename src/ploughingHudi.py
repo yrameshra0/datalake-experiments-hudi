2560952f91a94bc50888ac74007b785a9a64824b
Y Ramesh Rao <yrameshrao@gmail.com>
2023-06-21 11:04:16 +0530

    Quotes configuration failure fix

--- src/ploughingHudi.py
+++ src/ploughingHudi.py
@@ -53,21 +53,21 @@ sc = spark.sparkContext
 glueContext = GlueContext(sc)
 
 hudi_options = {
-    "hoodie.table.name": table_name,
+    'hoodie.table.name': table_name,
     "hoodie.datasource.write.storage.type": "COPY_ON_WRITE",
-    "hoodie.datasource.write.recordkey.field": "emp_id",
-    "hoodie.datasource.write.table.name": table_name,
-    "hoodie.datasource.write.operation": "upsert",
-    "hoodie.datasource.write.precombine.field": "ts",
+    'hoodie.datasource.write.recordkey.field': 'emp_id',
+    'hoodie.datasource.write.table.name': table_name,
+    'hoodie.datasource.write.operation': 'upsert',
+    'hoodie.datasource.write.precombine.field': 'ts',
 
-    "hoodie.datasource.hive_sync.enabled": "true",
-    "hoodie.datasource.hive_sync.mode": "hms",
-    "hoodie.datasource.hive_sync.sync_as_datasource": "false",
-    "hoodie.datasource.hive_sync.database": database_name,
-    "hoodie.datasource.hive_sync.table": table_name,
-    "hoodie.datasource.hive_sync.use_jdbc": "false",
-    "hoodie.datasource.hive_sync.partition_extractor_class": "org.apache.hudi.hive.MultiPartKeysValueExtractor",
-    "hoodie.datasource.write.hive_style_partitioning": "true"
+    'hoodie.datasource.hive_sync.enable': 'true',
+    "hoodie.datasource.hive_sync.mode":"hms",
+    'hoodie.datasource.hive_sync.sync_as_datasource': 'false',
+    'hoodie.datasource.hive_sync.database': database_name,
+    'hoodie.datasource.hive_sync.table': table_name,
+    'hoodie.datasource.hive_sync.use_jdbc': 'false',
+    'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor',
+    'hoodie.datasource.write.hive_style_partitioning': 'true',
 }
 
 #=================================================
